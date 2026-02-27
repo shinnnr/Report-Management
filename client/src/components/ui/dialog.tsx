7a10e6b715ddcce74b880a488602963b209ad4b8
shinnnr <lunaaraymond@gmail.com>
2026-02-27 23:40:01 +0800

    asd

--- client/src/components/ui/dialog.tsx
+++ client/src/components/ui/dialog.tsx
@@ -38,7 +38,7 @@ const DialogContent = React.forwardRef<
     <DialogPrimitive.Content
       ref={ref}
       className={cn(
-        "fixed inset-x-2 top-[50%] z-50 grid w-auto max-w-lg translate-x-0 translate-y-[-50%] gap-4 border bg-background px-4 sm:px-6 py-6 shadow-lg duration-200 rounded-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:inset-x-auto sm:left-[50%] sm:translate-x-[-50%] sm:max-w-lg sm:w-full",
+        "fixed left-2 right-2 sm:left-auto sm:right-auto sm:left-[50%] top-[50%] z-50 grid w-auto max-w-lg -translate-x-1/2 -translate-y-1/2 gap-4 border bg-background px-4 sm:px-6 py-6 shadow-lg duration-200 rounded-lg data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 sm:max-w-lg",
         className
       )}
       {...props}
